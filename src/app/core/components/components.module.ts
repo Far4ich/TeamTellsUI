8a6c9bdaac07a301000baada6de1076410e2913f
Firdavsi <firdavsinurov326@gmail.com>
2023-10-10 16:49:49 +0300

    Resolve conflict

--- src/app/core/components/components.module.ts
+++ src/app/core/components/components.module.ts
@@ -1,19 +1,42 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
+import { MainContentCardComponent } from './main-content-card/main-content-card.component';
+import { MenuComponent } from './menu/menu.component';
+import { EmployeeItemComponent } from './employee-item/employee-item.component';
+import { DepartmentComponent } from './department/department.component';
+import { ButtonRedComponent } from './button-red/button-red.component';
+import { MenuItemComponent } from './menu/menu-item/menu-item.component';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { ButtonBackComponent } from './button-back/button-back.component';
 import { InputFieldComponent } from './input-field/input-field.component';
 
 @NgModule({
   declarations: [
     CardComponent,
+    MainContentCardComponent,
+    MenuComponent,
+    EmployeeItemComponent,
+    DepartmentComponent,
+    ButtonRedComponent,
+    MenuItemComponent,
+    ButtonBackComponent,
     InputFieldComponent,
   ],
   exports: [
     CardComponent,
+    MainContentCardComponent,
+    MenuComponent,
+    EmployeeItemComponent,
+    DepartmentComponent,
+    ButtonRedComponent,
+    ButtonBackComponent,
     InputFieldComponent,
   ],
   imports: [
     CommonModule,
+    AppRoutingModule
   ]
 })
+
 export class ComponentsModule { }
